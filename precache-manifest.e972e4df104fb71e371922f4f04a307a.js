23a444cf2d70a12c110bb3d2431c2a968299aa97
taeukme <taeukme@users.noreply.github.com>
2020-07-09 04:19:22 +0000

    deploy: e7c75febf3cecf4802a7e7f9de43c790bceb0440

---
+++ precache-manifest.e972e4df104fb71e371922f4f04a307a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202dde5116873348e66fcff30d81d626",
+    "url": "/index.html"
+  },
+  {
+    "revision": "533791778e765098d710",
+    "url": "/static/css/main.f0045cab.chunk.css"
+  },
+  {
+    "revision": "2d9a5a610b6124883fc5",
+    "url": "/static/js/2.4f083686.chunk.js"
+  },
+  {
+    "revision": "ab38a95fb47bf3b6cd804889ab6f8ef8",
+    "url": "/static/js/2.4f083686.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "533791778e765098d710",
+    "url": "/static/js/main.10da3299.chunk.js"
+  },
+  {
+    "revision": "b51cd6df43632122f054",
+    "url": "/static/js/runtime-main.7a7f8f7e.js"
+  },
+  {
+    "revision": "146663f2f33372c6a97885d0e874ebe6",
+    "url": "/static/media/MaskMapIntro.146663f2.svg"
+  },
+  {
+    "revision": "b988c10966c28ad3078daec2313485bf",
+    "url": "/static/media/MaskMapLogo.b988c109.svg"
+  }
+]);
